f3bb91a9376a0e6f2df2c7717b1756f0216a0dfe
Clément METZ <metz.clement@gmail.com>
2026-01-07 01:15:44 +0800

    feat(matrix_ops): implemented matrix multiplication (by value)

--- libs/matrix_ops/src/mult.cu
+++ libs/matrix_ops/src/mult.cu
@@ -1,1 +1,18 @@
+#include <cstddef>
 #include <matrix_ops/ops.hpp>
+
+#include "array.hpp"
+#include "cuda_utils.cuh"
+
+__global__ void mult_kernel(int* out, const int* a, const int* b) {
+    const std::size_t index = blockDim.x * blockIdx.x + threadIdx.x;
+
+    out[index] = a[index] * b[index];
+}
+
+namespace matrix_ops {
+
+    matrix<int> mult(const matrix<int>& a, const matrix<int>& b) {
+        return perform_operation(a, b, mult_kernel);
+    }
+} // namespace matrix_ops
