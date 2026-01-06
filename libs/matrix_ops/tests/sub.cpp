187243c1f209d64da7ac6e427220ac7dd731ede0
Clément METZ <metz.clement@gmail.com>
2026-01-07 01:05:43 +0800

    test(matrix_ops): implemented testsuite for matrix substraction

---
+++ libs/matrix_ops/tests/sub.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <matrix_ops/ops.hpp>
+
+#include "test_parameters.hpp"
+
+TEST_P(MatrixOperTest, substraction) {
+    const MatrixOperTestParams param = GetParam();
+    const MatrixOperTestParams::matrix_t& a = param.a;
+    const MatrixOperTestParams::matrix_t& b = param.b;
+
+    const MatrixOperTestParams::matrix_t expected = a - b;
+    const MatrixOperTestParams::matrix_t actual = matrix_ops::sub(a, b);
+
+    ASSERT_TRUE(std::ranges::equal(expected.data(), actual.data()));
+}
