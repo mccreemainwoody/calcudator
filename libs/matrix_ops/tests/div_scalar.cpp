1dc7f5ba1f6f75eddcc373022b375ff17a064b0d
Clément METZ <metz.clement@gmail.com>
2026-01-08 21:03:47 +0800

    test(matrix_ops): implemented testsuite for matrix division (by scalar)

---
+++ libs/matrix_ops/tests/div_scalar.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <matrix_ops/ops.hpp>
+
+#include "test_parameters.hpp"
+
+TEST_P(MatrixScalarOperTest, division_scalar) {
+    const MatrixScalarOperTestParams param = GetParam();
+    const MatrixScalarOperTestParams::matrix_t& a = param.a;
+    const int k = param.k;
+
+    const MatrixScalarOperTestParams::matrix_t expected = a * k;
+    const MatrixScalarOperTestParams::matrix_t actual = matrix_ops::div(a, k);
+
+    ASSERT_TRUE(std::ranges::equal(expected.data(), actual.data()));
+}
